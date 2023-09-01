7a4e8b18833c4e41f460eeac809e537a0189277e
Josh Lazar <lazarjosh2@gmail.com>
2023-08-31 21:15:34 -0400

    updated chunk counter and comments

--- challenge.py
+++ challenge.py
@@ -22,8 +22,8 @@ with open(file_path, 'r') as f:
 
 
 # Here we initialize tiktoken so we can tokenize and encode
-# I used "p50k_base" because I am using the text-davinci-003 model
-encoding = tiktoken.get_encoding("p50k_base")
+# I used "cl100k_base" because I am using the gpt-3.5-turbo model
+encoding = tiktoken.get_encoding("cl100k_base")
 
 def process_chunk(chunk):
     # print(f"{len(chunk)} is the number of tokens in my text")
@@ -35,25 +35,24 @@ def process_chunk(chunk):
     # It is constructed to retrieve any specific demographic information based on responses by the Pateint
     # It will also summarize health information based on the pateints response
     # The \n which we also see in the transcripts.json is a newline character, which in this case will ensure the chunk contents appears on a new line below the prompt
-    prompt = "Extract demographic information about the patient, also include anything related to patients health from the following transcript:\n" + encoding.decode(chunk)
+    prompt = "Extract demographic information about the patient, including their age, location, health, and any relevant personal details, from the following conversation between a doctor and a patient:\n" + encoding.decode(chunk)
     
     # Call the OpenAI API
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=prompt,
-        max_tokens=150
-    )
+    conversation = [{"role": "user", "content": prompt}]
     
-    # We can access the models response by navigating to the choices list in the response dictionary, and then accessing the text
-    print(response.choices[0].text)
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=conversation
+    )
+
+    # This is the path I took to get to our message in the response dictionary 
+    assistant_response = response.choices[0].message['content']
+    print(assistant_response)
 
 
 
 # Define the maximum number of tokens per chunk
 max_tokens = 100
-
-begin_chunk_separator = "[%--- Begin Chunk ---%]"
-end_chunk_separator = "[%--- End Chunk ---%]"
 
 # Iterate over each conversation transcript
 for transcript in data['transcripts']:
@@ -63,6 +62,7 @@ for transcript in data['transcripts']:
     # Initialize variables for chunking
     current_chunk = []
     current_length = 0
+    current_chunk_number = 1
 
     for token in tokens:
         # Check if adding the token exceeds the max tokens per chunk
@@ -72,22 +72,40 @@ for transcript in data['transcripts']:
             current_length += 1
             
             # This is where I chose how to chunk the transcripts
-            # I decided to chunk the transcripts after one full statement/question by the Doctor, and one full statement by Patient
-            # After decoding the encoded chunk, I searched for b'\n' twice as after its found twice, one question and one response are found in the chunk and a new chunk should be started
+            # I decided to chunk the transcripts after one full statement/question by the Doctor, and one full statement/question by Patient
+            # After decoding the encoded chunk, I searched for b'?\n' and b'.\n' or b'.\n' twice, or b'?\n' twice as once this condition is met, one statement by each are found in the chunk and a new chunk should be started
             chunk_text = [encoding.decode_single_token_bytes(t) for t in current_chunk]
-            if chunk_text.count(b'\n') == 2:
+            if (b'?\n' in chunk_text and b'.\n' in chunk_text) or chunk_text.count(b'.\n') == 2 or chunk_text.count(b'?\n') == 2:
+
+                begin_chunk_separator = f"[%--- Begin Chunk [Chunk {current_chunk_number}] ---%]"
+                end_chunk_separator = f"[%--- End Chunk [Chunk {current_chunk_number}] ---%]"
+
                 # Process the current chunk
                 print(begin_chunk_separator)
                 process_chunk(current_chunk)
                 print(end_chunk_separator)
                 
                 # Start a new chunk
+                current_chunk_number += 1
                 current_chunk = []
                 current_length = 0
+                
+        else:
+            
+            print(begin_chunk_separator)
+            process_chunk(current_chunk)
+            print(end_chunk_separator)
+
+            current_chunk = [token]
+            current_length = 1
+            current_chunk_number += 1
+
     
     # Process the last chunk in the transcript
+    
     print(begin_chunk_separator)
     process_chunk(current_chunk)
     print(end_chunk_separator)
+    
 
 
